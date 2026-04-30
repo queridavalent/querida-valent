f53c23ab736768a5973655e90b96daaf254c6c93
queridavalent <levalentimcontato@gmail.com>
2026-04-29 21:35:05 -0300

    Update frete.js

--- netlify/functions/frete.js
+++ netlify/functions/frete.js
@@ -1,6 +1,5 @@
 exports.handler = async (event) => {
   const { cep } = JSON.parse(event.body);
-
   const response = await fetch('https://melhorenvio.com.br/api/v2/me/shipment/calculate', {
     method: 'POST',
     headers: {
@@ -12,18 +11,9 @@ exports.handler = async (event) => {
     body: JSON.stringify({
       from: { postal_code: '01415002' },
       to: { postal_code: cep },
-      products: [{
-        id: '1',
-        width: 15,
-        height: 5,
-        length: 20,
-        weight: 0.3,
-        insurance_value: 30,
-        quantity: 1
-      }]
+      products: [{ id: '1', width: 15, height: 5, length: 20, weight: 0.3, insurance_value: 30, quantity: 1 }]
     })
   });
-
   const data = await response.json();
   return {
     statusCode: 200,
